587bdd13f54489bf7b8c5a5fbf79deedd2404674
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2009-07-18 11:43:25 +0200

    Some structural reorganization in the project.

--- L0KVODispatcher+Test.m
+++ L0KVODispatcher+Test.m
@@ -6,8 +6,28 @@
 //  Copyright 2009 __MyCompanyName__. All rights reserved.
 //
 
-#import "L0KVODispatcher+Test.h"
+#import <SenTestingKit/SenTestingKit.h>
+#import <Foundation/Foundation.h>
+#import "MuiKit.h"
 
+@interface L0KVODispatcher_Test : SenTestCase {
+	L0KVODispatcher* dispatcher;
+	NSMutableArray* anArrayRelationship;
+	NSMutableSet* aSetRelationship;
+	
+	NSInteger countOfInsertions, lastIndexOfInsertion;
+	NSInteger countOfRemovals, lastIndexOfRemoval;
+	NSInteger countOfReplacements, lastIndexOfReplacement;
+	BOOL performedReplInsertion, performedReplRemoval;
+}
+
+@property(readonly) NSArray* anArrayRelationship;
+@property(readonly) NSSet* aSetRelationship;
+
+- (void) testArrayDispatch;
+- (void) testSetDispatch;
+
+@end
 
 @implementation L0KVODispatcher_Test
 
